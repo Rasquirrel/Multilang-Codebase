cee458c9c6d07b7b44cf0c0237eb24f0ebfb0756
Carlos_Gabriel-Carmo <carlosgabriel03082005@gmail.com>
2023-05-06 16:35:32 -0300

    input_simples

---
+++ rust/simples_input/target/debug/deps/simples_input-16c03224cfd5be89.d
@@ -0,0 +1,5 @@
+/home/carlosgabrielnn/Documentos/GitHub/estudando_rust/simples_input/target/debug/deps/simples_input-16c03224cfd5be89.rmeta: src/main.rs
+
+/home/carlosgabrielnn/Documentos/GitHub/estudando_rust/simples_input/target/debug/deps/simples_input-16c03224cfd5be89.d: src/main.rs
+
+src/main.rs:
---
+++ rust/simples_input/target/debug/deps/simples_input-50e3196ba2dd7c78.d
@@ -0,0 +1,5 @@
+/home/carlosgabrielnn/Documentos/GitHub/estudando_rust/simples_input/target/debug/deps/simples_input-50e3196ba2dd7c78.rmeta: src/main.rs
+
+/home/carlosgabrielnn/Documentos/GitHub/estudando_rust/simples_input/target/debug/deps/simples_input-50e3196ba2dd7c78.d: src/main.rs
+
+src/main.rs:
